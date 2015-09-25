2c7a42a5300e6af2921b6e3f2a58d0693bd9b913
Bobbie <Bobbie@Bobbie>
2015-09-25 04:31:18 +0200

    Created

---
+++ src/java/servlet/myServlet.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Bobbie
+ */
+@WebServlet(name = "myServlet", urlPatterns = {"/api/quote/*"})
+public class myServlet extends HttpServlet {
+
+    private Map<Integer, String> quotes = new HashMap() {
+        {
+            put(1, "Friends are kisses blown to us by angels");
+            put(2, "Do not take life too seriously. You will never get out of it alive");
+            put(3, "Behind every great man, is a woman rolling her eyes");
+        }
+    };
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    void doPut() {
+    }
+
+    void doDelete() {
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String[] parts = request.getRequestURI().split("/");
+        String parameter = null;
+        if (parts.length == 5) {
+            parameter = parts[4];
+        }
+        JsonObject quote = new JsonObject();
+        int key = Integer.parseInt(parameter); //Get the second quote
+        quote.addProperty("quote", quotes.get(key));
+        String jsonResponse = new Gson().toJson(quote);
+        response.getWriter().print(jsonResponse);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
